e399f067c90c7d68d36c3d7a3f873b77235c5c99
H J <qkrwlgml33@gmail.com>
2023-12-19 21:21:26 +0900

    로그인 오류 수정

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from user.models import User
 from user.serializers import UserSerializer
+from django.contrib.auth import authenticate
 
 class SignUpView(APIView):
     def post(self, request):
@@ -17,12 +18,23 @@ class SignUpView(APIView):
         return HttpResponse('회원가입이 완료되었습니다.')
 
 class LogInView(APIView):
-    def post(self, request):
-        user_id = request.data.get("id")
-        password = request.data.get("password")
+    def auth(self, user_id: str, password: str):
+        # user_id, password 맞나?
         try:
-            login_data = User.objects.get(email=user_id, password=password)
+            user = User.objects.get(mail=user_id, password=password)
+        except User.DoesNotExist:
+            return None
+        return user
+
+    def post(self, request):
+        x = request.data
+        user_id = request.data.get("user_id")
+        password = request.data.get("password")
+
+        user = self.auth(user_id, password)
+        if user is not None:
+            login_data = User.objects.get(mail=user_id, password=password)
             serializer = UserSerializer(login_data)
-        except Exception as e:
+            return Response(serializer.data, status=200)
+        else:
             return HttpResponse('아이디 및 비밀번호를 확인해주세요.', status=400)
-        return Response(serializer.data, status=200)
